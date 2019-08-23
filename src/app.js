28f23f6d2df6cb619be41341c3a71bc2dc3f49f5
Julianb12 <julianb1212@gmail.com>
2019-08-23 16:10:07 -0500

    functionality working

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const bookmarksRouter = require('./bookmark/router')
+
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
+
+app.use(bookmarksRouter)
+app.get('/', (req, res) => {
+  res.send('/bookmarks to view bookmarks')
+})
+
+
+
+module.exports = app
---
+++ src/store.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    id: 'a0bbe725-kk97-4950-81f3-j8364hdjksuw',
+    title: 'Time',
+    description: 'News',
+    url: 'http://www.time.com',
+    rating: 2
+  },
+  {
+    id: 'l7dy-908jkdud-abd4-bd99-4ce272c45d6a',
+    title: 'Reddit',
+    description: 'website',
+    url: 'http://www.reddit.com',
+    rating: 3
+  },
+  {
+    id: '7baoob8e-bbc1-44d5-9cbb-dea03a269058',
+    title: 'Google',
+    description: 'search engine',
+    url: 'http://www.google.com',
+    rating: 5
+  },
+  
+  
+];
